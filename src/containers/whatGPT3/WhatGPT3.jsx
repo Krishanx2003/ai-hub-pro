1775601b50517366715654bd2bcd9eb7ef6aa12f
Krishanx2003 <108981203+Krishanx2003@users.noreply.github.com>
2023-02-27 16:46:57 +0530

    features added

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -5,16 +5,16 @@ import './whatGPT3.css';
 const WhatGPT3 = () => (
   <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
     <div className="gpt3__whatgpt3-feature">
-      <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+      <Feature title="At AIHubPro" text="With our expertly designed courses and immersive learning experience, you'll gain the knowledge and skills to navigate the exciting world of AI. Join our community of AI enthusiasts today and be a part of the change that will shape the future!" />
     </div>
     <div className="gpt3__whatgpt3-heading">
       <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
       <p>Explore the Library</p>
     </div>
     <div className="gpt3__whatgpt3-container">
-      <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
-      <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
-      <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+      <Feature title="Oppurtunity" text="We recognize the potential of AI to transform every industry, from healthcare to finance, manufacturing to transportation, and beyond." />
+      <Feature title="Education" text="Our courses are free, ensuring that anyone, anywhere can access quality AI education. We are committed to building a world where AI is used ethically, responsibly, and for the betterment of humanity." />
+      <Feature title="Community" text="We also offer a community of AI enthusiasts, where you can connect with like-minded individuals, share ideas, and collaborate on projects. " />
     </div>
   </div>
 );
